5ba36e7def81030e992038d3028e245fd5e89dec
unknown <aonu4u@gmail.com>
2022-03-16 01:23:18 +0530

    added CV structure to UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SkillsComponent } from './Components/skills/skills.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ResumeComponent } from './Components/resume/resume.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { HomeComponent } from './Components/home/home.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { PortfolioComponent } from './Components/portfolio/portfolio.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsComponent,
+    AboutComponent,
+    ResumeComponent,
+    ContactComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
