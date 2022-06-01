01ed6859fdd06a16e5470ba98ad660a4716e2c92
unknown <aonu4u@gmail.com>
2022-06-01 17:43:48 +0530

    Dynamic navigation menus

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -7,22 +7,73 @@ import { fromEvent, Observable, Subscription } from 'rxjs';
 })
 export class HeaderComponent implements OnInit, OnDestroy {
 
+  scrollObservable: Observable<Event>
+  scrollSubscription: Subscription
+
   resizeObservable: Observable<Event>
   resizeSubscription: Subscription
 
   showNav = window.innerWidth > 1199
 
+  components = [
+    {
+      name: 'Home',
+      sectionId: 'home',
+      active: false,
+      menuIconClass: 'bx bx-home'
+    },
+    {
+      name: 'About',
+      sectionId: 'about',
+      active: false,
+      menuIconClass: 'bx bx-user'
+    },
+    {
+      name: 'Skills',
+      sectionId: 'skills',
+      active: false,
+      menuIconClass: 'bi bi-tools'
+    },
+    {
+      name: 'Resume',
+      sectionId: 'resume',
+      active: false,
+      menuIconClass: 'bx bx-file-blank'
+    },
+    {
+      name: 'Portfolio',
+      sectionId: 'portfolio',
+      active: false,
+      menuIconClass: 'bx bx-book-content'
+    },
+    {
+      name: 'Contact',
+      sectionId: 'contact',
+      active: false,
+      menuIconClass: 'bx bx-envelope'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+    this.updateComponents(this.components)
     this.resizeObservable = fromEvent(window,'resize')
     this.resizeSubscription = this.resizeObservable.subscribe(() => {
+      this.updateComponents(this.components)
       this.showNav = window.innerWidth > 1199
+    })
+
+    this.scrollObservable = fromEvent(window,'scroll')
+    this.scrollSubscription = this.scrollObservable.subscribe(() => {
+      this.updateComponents(this.components)
+      console.log(this.components)
     })
   }
 
   ngOnDestroy(): void {
     this.resizeSubscription.unsubscribe()
+    this.scrollSubscription.unsubscribe()
   }
 
   openFacebook(): void {
@@ -43,5 +94,14 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   onMenuClick(): void {
     if(window.innerWidth <= 1199 && this.showNav) this.showNav = false 
+  }
+
+  updateComponents(components: any): void {
+    components.forEach((component: any,index: number) => {
+      let componentElement = document.getElementById(component.sectionId)
+      let positionY = componentElement?.getBoundingClientRect().y
+      component.active = positionY !== undefined && positionY <= (2*(window.innerHeight)/5)
+      if(index > 0 && component.active) components[index-1].active = false
+    });
   }
 }
